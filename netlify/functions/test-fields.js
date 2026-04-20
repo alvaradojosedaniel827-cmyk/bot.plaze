7dd6e4346edd2c7916e0e681b2d7af8f5965b826
alvaradojosedaniel827-cmyk <alvaradojosedaniel827@gmail.com>
2026-04-20 14:29:27 -0500

    fix test-fields limit

--- netlify/functions/test-fields.js
+++ netlify/functions/test-fields.js
@@ -7,19 +7,24 @@ exports.handler = async function(event, context) {
   };
 
   try {
-    const response = await fetch('https://api.systeme.io/api/contacts?limit=10', {
+    const url = 'https://api.systeme.io/api/contacts?limit=10';
+    console.log('Fetching:', url);
+
+    const response = await fetch(url, {
       method: 'GET',
       headers: {
         'X-API-Key': 'jtelcfoiu6l462z9ic4w2808jdm4rt44d7evx2rjdyky2tqm1kflh5byi0mbakey'
       }
     });
 
-    const data = await response.json();
+    const text = await response.text();
+    console.log('Response status:', response.status);
+    console.log('Response:', text);
 
     return {
       statusCode: 200,
       headers,
-      body: JSON.stringify(data, null, 2)
+      body: text
     };
   } catch (error) {
     return {
